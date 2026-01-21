0dee1cc0b3155f63643d95b6dda14a2d21cae56f
LuckyMittenGaming <wsopamatt@gmail.com>
2026-01-20 21:57:21 -0500

    Add utility functions for token generation and timestamp

--- util.js
+++ util.js
@@ -1,1 +1,12 @@
+const crypto = require("crypto");
+
+function generateToken() {
+  return crypto.randomBytes(18).toString("base64url");
+}
+
+function now() {
+  return new Date().toISOString();
+}
+
+module.exports = { generateToken, now };
 
